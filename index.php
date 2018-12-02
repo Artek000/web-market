3f48f8d96f9f65126fe77dc5ac2db24283565e9b
Artem <1111255555@mail.ru>
2018-12-02 22:47:17 +0300

    v0.031

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Интернет Магазин</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Raleway+Dots" rel="stylesheet">
     <link href="/css/main-style.css" rel="stylesheet" type="text/css"/>
 
   </head>
@@ -14,11 +15,11 @@
         </div>
           <div id="menu-bar">
           <ul class="menu">
-            <li>Features</li>
-            <li>About</li>
-            <li>Pricing</li>
-            <li>Reviews</li>
-            <li>Contact</li>
+            <li><a href="#">Features</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Reviews</a></li>
+            <li><a href="#">Contact</a></li>
           </ul>
         </div>
         </div>
@@ -26,7 +27,21 @@
     </header>
 
     <div class="trial-form">
-      
+      <div class="trial-main">
+
+        <div class="left-side">
+          <div id="trial-img"><img src="/img/Logo-trial-block.png"></div>
+          <div id="main-text"><span style="text-transform: uppercase">MODERN AXURE TEMPLATE FOR BEAUTIFUL PROTOTYPES</span></div>
+          <div id="trial-line"><img src="/img/line.png"></div>
+          <div id="text"><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.</span></div>
+          <button id="download-button">Download</button>
+        </div>
+
+        <div class="right-side">
+
+        </div>
+
+      </div>
     </div>
 
 
