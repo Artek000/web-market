ac4247bdfa8ebf88064599fc0083239e8d008e4f
Artem <1111255555@mail.ru>
2018-12-02 21:08:29 +0300

    v0.03

--- index.php
+++ index.php
@@ -3,8 +3,33 @@
   <head>
     <meta charset="utf-8">
     <title>Интернет Магазин</title>
+    <link href="/css/main-style.css" rel="stylesheet" type="text/css"/>
+
   </head>
   <body>
+    <header>
+      <div class="manage-panel">
+          <div id="logo">
+          <img src="/img/Logo.png">
+        </div>
+          <div id="menu-bar">
+          <ul class="menu">
+            <li>Features</li>
+            <li>About</li>
+            <li>Pricing</li>
+            <li>Reviews</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    </header>
+
+    <div class="trial-form">
+      
+    </div>
+
+
 
   </body>
 </html>
